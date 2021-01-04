ebc5a3874c212c8f20fbed6be644cce581f3bb5d
TaylorPlusPlus <Taylor1216@live.com>
2021-01-04 11:01:19 -0500

    Client Completed

--- PalindromeClient/Program.cs
+++ PalindromeClient/Program.cs
@@ -7,8 +7,35 @@ namespace PalindromeClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool UserHasNotExited = true;
+            string UserInput = "";
+            PalindromeChecker Logic = new PalindromeChecker();
+
+            while(UserHasNotExited)
+            {
+                 Console.WriteLine("Enter a string to see if it is a palindrome or enter 1 to exit: ");
+                 UserInput = Console.ReadLine();
+                 
+                if(String.Equals(UserInput, "1"))
+                {
+                    UserHasNotExited = false;
+                }else
+                {
+                    if(Logic.CheckPalindrome(Logic.WhiteSpaceRemover(UserInput)))
+                    {
+                        Console.WriteLine("The String you have entered IS a palindrome");
+                    }else
+                    {
+                        Console.WriteLine("The String you have entered is NOT a palindrome");
+                    }
+                }
+            }
+           
+
+
+
             
+
         }
     }
 }
